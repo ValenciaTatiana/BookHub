b91db8ebb3c384ce0d20ff61c4228f97e39baf3d
Andres Grajales <andresgrajales1133144@correo.itm.edu.co>
2025-11-12 23:18:18 -0500

    Agregado módulo de reportes - entrega 3

---
+++ src/main/java/com/bookhub/ui/reportes/repository/ReporteRepository.java
@@ -0,0 +1,9 @@
+package com.bookhub.reportes.repository;
+
+import com.bookhub.reportes.dto.ReporteSistema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReporteRepository extends JpaRepository<ReporteSistema, Long> {
+}
