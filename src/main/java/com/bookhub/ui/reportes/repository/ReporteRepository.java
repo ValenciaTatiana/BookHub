3fdec1166197e58013649e69aeb77fbf2168833c
JesusM-25 <jesusmeza330394@correo.itm.edu.co>
2025-11-25 10:52:38 -0500

    Eliminar Swing UI y las clases relacionadas de BookHub
    Se transfirieron las carpetas swing al frontend

--- src/main/java/com/bookhub/ui/reportes/repository/ReporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookhub.reportes.repository;
-
-import com.bookhub.reportes.dto.ReporteSistema;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReporteRepository extends JpaRepository<ReporteSistema, Long> {
-}
